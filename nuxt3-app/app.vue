365607c68ea9de4d540a973c994c46970a712ad7
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2024-09-22 17:16:47 +0900

    変数で制御

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+  const message = ref<string>("Hello World!!");
+</script>
 <template>
   <div>
-      <h1>Hello World!</h1>
+      <h1>{{ message }}</h1>
   </div>
 </template>
